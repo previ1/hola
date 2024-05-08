5f68085445641ed1659cd8b1655b63f90fc173ee
previ1 <santipre10gmail.com>
2024-05-08 16:40:26 -0300

    TP1

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    private static void Main()
+    {
+        Console.WriteLine("Bienvenido a aerolineas previ por favor ingrese sus datos");
+        Console.WriteLine("Nombre:");
+        string Nombre = Console.ReadLine();
+        Console.WriteLine("DNI:");
+        int DNI = int.Parse(Console.ReadLine());
+        Console.WriteLine("Edad:");
+        int Edad = int.Parse(Console.ReadLine());
+        Console.WriteLine("Destino:");
+        string Destino = Console.ReadLine();
+        
+        
+       
+
+
+    }
+}
